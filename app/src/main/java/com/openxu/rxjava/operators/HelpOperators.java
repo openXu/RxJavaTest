a4aafbff75287c741136ab66c0e76308c297eb5c
openXu <openXu@yeah.net>
2016-06-12 17:04:15 +0800

    辅助操作符

--- app/src/main/java/com/openxu/rxjava/operators/HelpOperators.java
+++ app/src/main/java/com/openxu/rxjava/operators/HelpOperators.java
@@ -5,14 +5,20 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Notification;
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
+import rx.schedulers.TimeInterval;
+import rx.schedulers.Timestamped;
 
 /**
  * author : openXu
@@ -54,7 +60,19 @@ public class HelpOperators extends OperatorsBase {
      * RxJava的实现是 delay和delaySubscription
      */
     private void op_Delay(TextView textView){
-
+        Observable<Integer> obs = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for(int i =0;i<5;i++){
+                    if(i>2){
+                        subscriber.onError(new Throwable("VALUE TO MAX"));
+                    }
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.computation());
+        
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         /*
          * Delay操作符让原始Observable在发射每项数据之前都暂停一段指定的时间段。
@@ -63,30 +81,42 @@ public class HelpOperators extends OperatorsBase {
          * 注意：delay不会平移onError通知，它会立即将这个通知传递给订阅者，同时丢弃任何待发射的onNext通知。
          * 然而它会平移一个onCompleted通知。
          */
-        Log.v(TAG, "start:" + sdf.format(new Date()));
-        Observable.interval(100, TimeUnit.MILLISECONDS)
-                .take(3)
-                .subscribeOn(Schedulers.newThread())
-                .delay(2, TimeUnit.SECONDS)
-                .subscribe(new Action1<Long>() {
-            @Override
-            public void call(Long aLong) {
-                Log.v(TAG, "delay onNext:" + sdf.format(new Date())+":"+aLong);
-            }
-        });
+        Log.v(TAG, "delay start:" + sdf.format(new Date()));
+        obs.delay(2, TimeUnit.SECONDS)
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "delay onCompleted" + sdf.format(new Date()));
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "delay onError"+e.getMessage());
+                    }
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.v(TAG, "delay onNext:" + sdf.format(new Date())+"->"+integer);
+                    }
+                });
 
         /*
          * delaySubscription:延迟订阅原始Observable
          */
-        Log.v(TAG, "start:" + sdf.format(new Date()));
-        Observable.interval(100, TimeUnit.MILLISECONDS)
-                .take(3)
-                .subscribeOn(Schedulers.newThread())
-                .delaySubscription(2, TimeUnit.SECONDS)
-                .subscribe(new Action1<Long>() {
+        Log.v(TAG, "delaySubscription start:" + sdf.format(new Date()));
+        obs.delaySubscription(2, TimeUnit.SECONDS)
+                .subscribe(new Subscriber<Integer>() {
                     @Override
-                    public void call(Long aLong) {
-                        Log.v(TAG, "delaySubscription onNext:" + sdf.format(new Date())+":"+aLong);
+                    public void onCompleted() {
+                        Log.v(TAG, "delaySubscription onCompleted" + sdf.format(new Date()));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "delaySubscription onError"+e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.v(TAG, "delaySubscription onNext:" + sdf.format(new Date())+"->"+integer);
                     }
                 });
 
@@ -199,10 +229,7 @@ public class HelpOperators extends OperatorsBase {
         Observable<Integer> obs = Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
             public void call(Subscriber<? super Integer> subscriber) {
-                for(int i =0;i<5; i++){
-                    if(i>2){
-                        new RuntimeException("TO MAX VALUE");
-                    }
+                for(int i = 0;i<3; i++){
                     subscriber.onNext(i);
                 }
                 subscriber.onCompleted();
@@ -221,7 +248,7 @@ public class HelpOperators extends OperatorsBase {
                     public void onError(Throwable e) {
                         Log.v(TAG,"onError:"+e.getMessage());
                     }
-
+                    //将所有的消息封装成Notification后再发射出去
                     @Override
                     public void onNext(Notification<Integer> integerNotification) {
                         Log.v(TAG,"onNext:"+integerNotification.getKind()+":"+integerNotification.getValue());
@@ -230,16 +257,35 @@ public class HelpOperators extends OperatorsBase {
 
         Log.v(TAG, "dematerialize-----------");
         obs.materialize()
-                .dematerialize()
-                .subscribe(i->Log.v(TAG, "deMeterialize:"+i));
+            //将Notification逆转为普通消息发射
+           .dematerialize()
+           .subscribe(integer->Log.v(TAG, "deMeterialize:"+integer));
     }
 
 
     /**
-     *
+     * SubscribeOn：指定Observable自身在哪个调度器上执行（即在那个线程上运行）
+     * ObserveOn： 指定一个观察者在哪个调度器上观察这个Observable
      */
-    private void op_OberveOn(TextView textView){
+    private void op_ObserveOn_SubscribeOn(TextView textView){
+        Observable<Integer> obs = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                Log.v(TAG, "on subscrib:" + Thread.currentThread().getName());
+                subscriber.onNext(1);
+                subscriber.onCompleted();
+            }
+        });
 
+        //在新建子线程中执行，在主线程中观察
+        obs.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(i ->  Log.v(TAG, "mainThread-onNext:" + Thread.currentThread().getName()));
+
+        obs.delaySubscription(2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.computation())  //用于计算任务，如事件循环或和回调处理
+                .observeOn(Schedulers.immediate())      //在当前线程立即开始执行任务
+                .subscribe(i ->  Log.v(TAG, "immediate-onNext:" + Thread.currentThread().getName()));
     }
 
     /**
@@ -256,45 +302,203 @@ public class HelpOperators extends OperatorsBase {
 
     }
 
-    /**
-     *
-     */
-    private void op_SubscribeOn(TextView textView){
-
-    }
 
     /**
-     *
+     *  TimeInterval操作符拦截原始Observable发射的数据项，替换为两个连续发射物之间流逝的时间长度
      */
     private void op_TimeInterval(TextView textView){
-
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i <= 3; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .timeInterval()
+                .subscribe(new Subscriber<TimeInterval<Integer>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "onCompleted");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "onError:"+e.getMessage());
+                    }
+                    @Override
+                    public void onNext(TimeInterval<Integer> integerTimeInterval) {
+                        Log.v(TAG, "onNext:"+integerTimeInterval.getValue()+
+                                "-"+integerTimeInterval.getIntervalInMilliseconds());
+                    }
+                });
     }
 
     /**
-     *
+     * 如果原始Observable过了指定的一段时长没有发射任何数据，
+     * Timeout操作符会以一个onError通知终止这个Observable，
+     * 或者继续一个备用的Observable。
      */
     private void op_Timeout(TextView textView){
+        Observable<Integer> obs = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i <= 3; i++) {
+                    try {
+                        Thread.sleep(i * 100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        });
 
+        //发射数据时间间隔超过200ms超时
+        obs.timeout(200, TimeUnit.MILLISECONDS)
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "onCompleted");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "onError:"+e);
+                    }
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.v(TAG, "onNext:"+integer);
+                    }
+        });
+
+        //发射数据时间间隔超过200ms超时,超时后开启备用Observable
+        obs.timeout(200, TimeUnit.MILLISECONDS, Observable.just(10,20))
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "onCompleted");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "onError:"+e);
+                    }
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.v(TAG, "onNext:"+integer);
+                    }
+                });
     }
 
     /**
-     *
+     * 把Observable发射的数据重新包装了一下，将数据发射的时间打包一起发射出去，
+     * 这样观察者不仅能得到数据，还能得到数据的发射时间
      */
     private void op_Timestamp(TextView textView){
-
+        Observable.just(1,2,3)
+                .timestamp()
+                .subscribe(new Subscriber<Timestamped<Integer>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "onCompleted");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "onError:"+e.getMessage());
+                    }
+                    @Override
+                    public void onNext(Timestamped<Integer> integerTimestamped) {
+                        Log.v(TAG, "onNext:"+integerTimestamped.getValue()+
+                                ",time:"+integerTimestamped.getTimestampMillis());
+                    }
+                });
     }
 
+    class MyObject{
+        public void release(){
+            Log.v(TAG, "object resource released");
+        }
+    }
     /**
-     *
+     * Using操作符指示Observable创建一个只在它的生命周期内存在的资源，
+     * 当Observable终止时这个资源会被自动释放。
      */
     private void op_Using(TextView textView){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        Observable<Long> obs = Observable.using(
+                //一个用于 创建一次性资源的工厂函数
+                new Func0<MyObject>() {
+                    @Override
+                    public MyObject call() {
+                        return new MyObject();
+                    }
+                }
+                //一个用于创建Observable的工厂函数，这个函数返回的Observable就是最终被观察的Observable
+                , new Func1<MyObject, Observable<Long>>() {
+                    @Override
+                    public Observable<Long> call(MyObject obj) {
+                        //创建一个Observable，3s之后发射一个简单的数字0
+                        return Observable.timer(3000,TimeUnit.MILLISECONDS);
+                    }
+                }
+                //一个用于释放资源的函数，当Func2返回的Observable执行完毕之后会被调用
+                ,new Action1<MyObject>(){
+                    @Override
+                    public void call(MyObject o) {
+                       o.release();
+                    }
+                }
+        );
 
+        Subscriber subscriber = new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+                Log.v(TAG, "onCompleted:" + sdf.format(new Date()));
+            }
+            @Override
+            public void onError(Throwable e) {
+                Log.v(TAG, "onError:"+e.getMessage());
+            }
+            @Override
+            public void onNext(Long l) {
+                Log.v(TAG, "onNext:"+l);
+            }
+        };
+
+        Log.v(TAG, "start:" + sdf.format(new Date()));
+        obs.subscribe(subscriber);
     }
 
     /**
-     *
+     * 将Observable转换为另一个对象或数据结构
      */
     private void op_To(TextView textView){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        //toList:阻塞等待原Observable发射完毕后，将发射的数据转换成List发射出去
+        Log.v(TAG, "toList start:" + sdf.format(new Date()));
+        Observable.interval(1000,TimeUnit.MILLISECONDS)
+                .take(3)
+                .toList()
+                .subscribe(new Subscriber<List<Long>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.v(TAG, "onCompleted");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.v(TAG, "onError:" + e.getMessage());
+                    }
+                    @Override
+                    public void onNext(List<Long> longs) {
+                        Log.v(TAG, "onNext:" + longs+" ->"+ sdf.format(new Date()));
+                    }
+                });
+
 
     }
 
