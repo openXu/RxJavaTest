d41675c0cccbd35eb0f1c38c9a4f450225b5c502
openXu <openXu@yeah.net>
2016-07-04 16:43:58 +0800

    调度器

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ RxJava学习
 [RxJava操作符(07-辅助操作)](http://blog.csdn.net/xmxkf/article/details/51658445)
 
 [RxJava操作符(08-条件和布尔操作)](http://blog.csdn.net/xmxkf/article/details/51671826)
+
+[RxJava操作符(09-算术/聚合操作&连接操作)](http://blog.csdn.net/xmxkf/article/details/51692493)
+
+[RxJava(10-操作符原理&自定义操作符)](http://blog.csdn.net/xmxkf/article/details/51791120)
+
+[RxJava(11-线程调度Scheduler)](http://blog.csdn.net/xmxkf/article/details/51821940)
--- app/src/main/java/com/openxu/rxjava/SchedulerActivity.java
+++ app/src/main/java/com/openxu/rxjava/SchedulerActivity.java
@@ -27,12 +27,6 @@ public class SchedulerActivity extends AppCompatActivity {
     Button btn2;
     @Bind(R.id.btn_3)
     Button btn3;
-    @Bind(R.id.btn_4)
-    Button btn4;
-    @Bind(R.id.btn_5)
-    Button btn_5;
-    @Bind(R.id.btn_6)
-    Button btn_6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +36,7 @@ public class SchedulerActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5, R.id.btn_6})
+    @OnClick({R.id.btn_1, R.id.btn_2, R.id.btn_3})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_1:
@@ -52,12 +46,7 @@ public class SchedulerActivity extends AppCompatActivity {
                 moreSubscribeOn();
                 break;
             case R.id.btn_3:
-                break;
-            case R.id.btn_4:
-                break;
-            case R.id.btn_5:
-                break;
-            case R.id.btn_6:
+                observeOn();
                 break;
         }
     }
@@ -152,7 +141,92 @@ public class SchedulerActivity extends AppCompatActivity {
          OnSubscribe -Thread[RxIoScheduler-2,5,main]
          onNext:1 -RxIoScheduler-2
          */
+
+        //多次subscribeOn的伪代码
+        /*
+        ...
+        //第3个subscribeOn产生的新线程
+        new Thread(){
+            @Override
+            public void run() {
+                Subscriber s1 = new Subscriber();
+                //第2个subscribeOn产生的新线程
+                new Thread(){
+                    @Override
+                    public void run() {
+                        Subscriber s2 = new Subscriber();
+                        //第1个subscribeOn产生的新线程
+                        new Thread(){
+                            @Override
+                            public void run() {
+                                Subscriber<T> s3 = new Subscriber<T>(subscriber) {
+                                    @Override
+                                    public void onNext(T t) {
+                                        subscriber.onNext(t);
+                                    }
+                                    ...
+                                };
+                                //①. 最后一个新观察者订阅原始Observable
+                                原始Observable.subscribe(s3);
+                                //②. 原始Observable将在此线程中发射数据
+
+                                //③. 最后一个新的观察者s3接受数据
+
+                                //④. s3收到数据后，直接发送给s2，s2收到数据后传给s1,...最后目标观察者收到数据
+                            }
+                        }.start();
+                    }
+                }.start();
+            }
+        }.start();
+        */
     }
+
+
+    private void observeOn() {
+        /*
+        Observable.just(100)
+                .map(integer -> {return "map1-"+integer;}) //主线程中接受数据
+                .observeOn(Schedulers.io())          //①. 切换
+                .map(integer -> {return "map2-"+integer;}) //io线程中接受数据，由①决定
+                .observeOn(Schedulers.computation()) //②. 切换
+                .map(integer -> {return "map3-"+integer;}) //computation线程中接受数据，由②决定
+                .observeOn(AndroidSchedulers.mainThread()) //③. 切换
+                .map(integer -> {return "map4-"+integer;}) //主线程中接受数据，由③决定
+                .subscribe(str -> logThread(str, Thread.currentThread())); //主线程中接受数据，由③决定
+           */
+        /*
+         输出：
+         onNext:map4-map3-map2-map1-100 -main
+         */
+
+
+        Observable.just(100)
+                .subscribeOn(Schedulers.computation())     //Computation线程中发射数据
+                .map(integer -> {return "map1-"+integer;}) //Computation线程中接受数据
+                .observeOn(Schedulers.io())          //②. 切换
+                .map(integer -> {return "map2-"+integer;}) //io线程中接受数据，由②决定
+                .observeOn(Schedulers.newThread())   //③. 切换
+                .map(integer -> {return "map3-"+integer;}) //newThread线程中接受数据，由③决定
+                .observeOn(AndroidSchedulers.mainThread()) //④. 切换
+                .delay(1000, TimeUnit.MILLISECONDS)        //主线程中接受数据，由④决定
+                .subscribe(str -> logThread(str, Thread.currentThread()));  //Computation线程中接受数据，由④决定
+
+        /*
+            说明：最后目标观察者将在Computation线程中接受数据，这取决于delay操作符，
+            delay操作符是在Computation线程中执行的，执行完后就会将数据发送给目标观察者。
+            而他上面的observeOn将决定于delay产生的代理观察者在主线程中接受数据
+         */
+
+
+        /*
+         输出：
+         onNext:map3-map2-map1-100 -RxComputationScheduler-3
+         */
+
+    }
+
+
 
 
 }
